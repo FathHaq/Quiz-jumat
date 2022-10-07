27ced91150fd9cc38a26c1bf857950e114b31bd9
Fath_Haq <miftahulk414@gmail.com>
2022-10-08 02:51:27 +0700

    first comit

---
+++ maxMin.js
@@ -0,0 +1,7 @@
+const deret = [5,4,9,10,60]
+const reverseDeret = deret.reverse()
+
+function maxMin () {
+	console.log('terbesar ', reverseDeret[0], ', ', 'terkecil ', deret[4]);
+}
+return maxMin()
